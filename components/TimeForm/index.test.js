0aa898eefe9dd990aabd8dfbe17aac5b52403b4d
HristoKachev <93873677+Kachev@users.noreply.github.com>
2023-04-03 09:11:35 +0200

    delete comment

--- components/TimeForm/index.test.js
+++ components/TimeForm/index.test.js
@@ -2,15 +2,6 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import TimeForm from ".";
 
-/* test("should render correct labels", () => {
-  render(<TimeForm />);
-  expect(screen.getByText("Datum:")).toBeInTheDocument();
-  expect(screen.getByText("Mitarbeiter")).toBeInTheDocument();
-  expect(screen.getByText("Von")).toBeInTheDocument();
-  expect(screen.getByText("Bis")).toBeInTheDocument();
-  expect(screen.getByText("Pause")).toBeInTheDocument();
-}); */
-
 describe("TimeForm", () => {
   test("should render a form with required fields and their associated labels", () => {
     render(<TimeForm />);
